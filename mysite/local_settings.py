64733713e97361a20381acb3fbb5862ba2d17434
RamonBecker <ramonbecker68@gmail.com>
2020-06-01 15:28:12 -0300

    add settings

---
+++ mysite/local_settings.py
@@ -0,0 +1,9 @@
+
+SECRET_KEY = '-*&ll3s*srj3)c8i@v#5wc&*mzggla5yb7$&z09%a=u+f075!oada1312'
+
+DEBUG = True
+
+DB_PASSWORD = '2622lbs1999'
+DB_USER = 'becker'
+DB_NAME = 'RamonBecker199@blog'
+DB_HOST = ''
