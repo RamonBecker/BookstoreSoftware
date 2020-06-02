f55bd903d415f82bb69b91576d6f0051b57b1102
RamonBecker <ramonbecker68@gmail.com>
2020-06-02 18:01:11 -0300

    Add git ignore local_settings

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -3,7 +3,7 @@ SECRET_KEY = '-*&ll3s*srj3)c8i@v#5wc&*mzggla5yb7$&z09%a=u+f075!oada1312'
 
 DEBUG = True
 
-DB_PASSWORD = '2622lbs1999'
+DB_PASSWORD = 'becker'
 DB_USER = 'becker'
-DB_NAME = 'RamonBecker199@blog'
+DB_NAME = 'blog'
 DB_HOST = ''
